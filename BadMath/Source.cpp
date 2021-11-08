a041ec384a3e0273982d4db3d80e36b5b2eb637b
Bleach Bo <zanebarhorst2@gmail.com>
2021-11-07 20:51:10 -0500

    This is the committed updated version of bad math fixing the 2 bugs causing the logical error in the program. The first bug occured due to lack of parenthases around the i1-i3. The second bug occured due to the fact that the variable n2 is being inputted twice.

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 float average(int i1, int i2, int i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	//The first bug occurs due to the lack of parenthases when computing the average of the 3 numbers. To fix this put i1-i3 in parenthases.
+	return (i1 + i2 + i3) / 3.0f;
 }
 
 int main()
@@ -13,7 +14,8 @@ int main()
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	//The second bug occurs due to the fact that the variable n2 is being inputted twice. To fix this replace the second n2 with n3 so the correct number is being inputted.
+	cout << "Third number: "; cin >> n3;
 
 	float a = average(n1, n2, n3);
 
